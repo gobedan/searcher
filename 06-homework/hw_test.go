5420f7fed549e5d89d60ad1a91b424af1294046e
Valentin Markin <jobedano@gmail.com>
2025-07-29 13:57:10 +0400

    hw6 complete with refactor

--- 06-homework/hw_test.go
+++ 06-homework/hw_test.go
@@ -2,21 +2,26 @@ package hw
 
 import "testing"
 
-func TestGeom_CalculateDistance(t *testing.T) {
+func TestGeom_Distance(t *testing.T) {
 	tests := []struct {
 		name         string
-		geom         Geom
+		points       [4]float64
 		wantDistance float64
 	}{
 		{
 			name:         "#1",
-			geom:         Geom{X1: 1, Y1: 1, X2: 4, Y2: 5},
+			points:       [4]float64{1, 1, 4, 5},
+			wantDistance: 5,
+		},
+		{
+			name:         "#2",
+			points:       [4]float64{-1, -1, 2, 3},
 			wantDistance: 5,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotDistance := tt.geom.CalculateDistance(); gotDistance != tt.wantDistance {
+			if gotDistance := Distance(tt.points[0], tt.points[2], tt.points[1], tt.points[3]); gotDistance != tt.wantDistance {
 				t.Errorf("Geom.CalculateDistance() = %v, want %v", gotDistance, tt.wantDistance)
 			}
 		})
