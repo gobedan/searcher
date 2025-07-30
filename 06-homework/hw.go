4be4177bc445eb33d524d9892632642db748af03
Valentin Markin <jobedano@gmail.com>
2025-07-30 14:35:38 +0400

    fix

--- 06-homework/hw.go
+++ 06-homework/hw.go
@@ -4,6 +4,6 @@ import (
 	"math"
 )
 
-func Distance(x1, x2, y1, y2 float64) float64 {
+func Distance(x1, y1, x2, y2 float64) float64 {
 	return math.Sqrt(math.Pow(x2-x1, 2) + math.Pow(y2-y1, 2))
 }
--- 06-homework/hw_test.go
+++ 06-homework/hw_test.go
@@ -21,7 +21,7 @@ func TestGeom_Distance(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if gotDistance := Distance(tt.points[0], tt.points[2], tt.points[1], tt.points[3]); gotDistance != tt.wantDistance {
+			if gotDistance := Distance(tt.points[0], tt.points[1], tt.points[2], tt.points[3]); gotDistance != tt.wantDistance {
 				t.Errorf("Geom.CalculateDistance() = %v, want %v", gotDistance, tt.wantDistance)
 			}
 		})
