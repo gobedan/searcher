71b7186d08699841c55992cc7999beaa48ce63f1
Valentin Markin <jobedano@gmail.com>
2025-09-02 18:56:07 +0400

    hw13 task1 complete

---
+++ 13-homework/cmd/webapp/webapp.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	api "go_search/13-homework/pkg/API"
+	"go_search/13-homework/pkg/scanner"
+	"log"
+	"net/http"
+)
+
+const depth = 2
+
+type server struct {
+	api *api.API
+}
+
+func main() {
+
+	urls := []string{"https://go.dev", "https://golang.org"}
+
+	echan := make(chan error, 10)
+	go scanner.ScanAll(urls, depth, echan)
+	go func() {
+		err := <-echan
+		fmt.Println(err)
+	}()
+
+	srv := server{api: api.New()}
+
+	err := http.ListenAndServe(":8080", srv.api.Router)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module go_search
 go 1.24.4
 
 require golang.org/x/net v0.42.0
+
+require github.com/gorilla/mux v1.8.1
